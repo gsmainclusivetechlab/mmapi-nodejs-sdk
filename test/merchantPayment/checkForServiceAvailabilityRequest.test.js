d9e4b5876f6494462a8b101e695d52bc5748be9d
pramishacpfingent <pramisha.cp@fingent.com>
2021-10-21 12:24:12 +0530

    add check for service availability api

---
+++ test/merchantPayment/checkForServiceAvailabilityRequest.test.js
@@ -0,0 +1,25 @@
+require('../test_helper');
+
+const client = require('../test_harness').client();
+
+const { CheckForServiceAvailabilityRequest } = mobileMoneyApi.merchantPayment;
+
+const checkForServiceAvailabilityRequest = async () => {
+  const request = new CheckForServiceAvailabilityRequest();
+
+  const response = await client.execute(request);
+  return response;
+}
+
+describe('Check For Service Availability Request', () => {
+  it('should return status 200 with the availability of the service (available, unavailable, degraded) ', async () => {
+    const response = await checkForServiceAvailabilityRequest();
+
+    expect(response.status).toBe(200);
+    expect(response.data.serviceStatus).toMatch('available');
+  });
+});
+
+module.exports = {
+  CheckForServiceAvailability: checkForServiceAvailabilityRequest
+}
