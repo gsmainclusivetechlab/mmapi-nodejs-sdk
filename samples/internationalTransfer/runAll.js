dc03e5386c65d3a9eb25e5ff669f06183c38ea18
pramishacpfingent <pramisha.cp@fingent.com>
2021-11-11 22:29:56 +0530

    international transfer apis added

--- samples/internationalTransfer/runAll.js
+++ samples/internationalTransfer/runAll.js
@@ -24,13 +24,95 @@ const usecase1 = async () => {
   await createAIntTransferTransaction('REF-1636533162268', undefined, true);
 }
 
+const usecase2 = async () => {
+  console.log("Perform an Bilateral International Transfer...");
+
+  console.log("POST Request a International Transfer Quotation")
+  await createANewQuotation(true);
+
+  console.log("POST Perform an International Transfer")
+  await createAIntTransferTransaction('REF-1636533162268', undefined, true);
+}
+
+const usecase3 = async () => {
+  console.log("Perform a International Transfer Reversal...");
+
+  console.log("POST Perform an International Transfer")
+  const { data: { serverCorrelationId } } = await createAIntTransferTransaction('REF-1636533162268', undefined, true);
+
+  console.log('GET Poll to Determine the Request State')
+  const { data: { objectReference } } = await viewARequestState(serverCorrelationId, true);
+
+  console.log('GET Perform a Merchant Payment Reversal')
+  await createAReversal(objectReference, true);
+}
+
+const usecase4 = async () => {
+  console.log("Obtain a Financial Service Provider Balance...");
+
+  console.log('GET Get an Account Balance')
+  await viewAccountBalance('accountid', '2000', true);
+}
+
+const usecase5 = async () => {
+  console.log("Retrieve Transactions for a Financial Service Provider...");
+
+  console.log('GET Retrieve a Set of Transactions for an Account')
+  await viewAccountSpecificTransaction('accountid', '2000', 0, 20);
+}
+
+const usecase6 = async () => {
+  console.log("Check for API Provider Service Availability...")
+
+  console.log('GET Check for Service Availability')
+  await checkApiAvailability(true);
+}
+
+const usecase7 = async () => {
+  console.log("Retrieve a Missing API Response from an API Provider...")
+
+  console.log('POST Perform an International Transfer');
+  const { config: { headers } } = await createAIntTransferTransaction('REF-1636533162268', undefined, true);
+
+  console.log('GET Retrieve a Missing Response');
+  const { data: { link } } = await viewAResponse(headers['X-CorrelationID'], true);
+
+  console.log('GET Retrieve a Missing Resource');
+  await viewAResource(link, true);
+}
+
 (async (usecase) => {
   switch (usecase) {
     case 1:
       await usecase1();
       break;
+    case 2:
+      await usecase2();
+      break;
+    case 3:
+      await usecase3();
+      break;
+    case 4:
+      await usecase4();
+      break;
+    case 5:
+      await usecase5();
+      break;
+    case 6:
+      await usecase6();
+      break;
+    case 7:
+      await usecase7();
+      break;
     default:
       await usecase1();
+      await usecase2();
+      await usecase3();
+      await usecase4();
+      await usecase5();
+      await usecase6();
+      await usecase7();
+      break;
   }
 })();
 
--- test/internationalTransfer.test.js
+++ test/internationalTransfer.test.js
@@ -48,4 +48,161 @@ describe('International Transfers', () => {
       });
     })
   });
+
+  describe('Perform an Bilateral International Transfer', () => {
+    describe('POST Request a International Transfer Quotation', () => {
+      it('should return the request state object with status 202 to indicate that the request is pending', async () => {
+        const response = await createANewQuotation(false);
+
+        expect(response.status).toBe(202);
+        expect(response.data).toHaveProperty('status');
+        expect(response.data.status).toBe('pending');
+        expect(response.data).toHaveProperty('serverCorrelationId');
+        expect(response.data).toHaveProperty('notificationMethod');
+        expect(response.data.notificationMethod).toBe('callback');
+      });
+    })
+
+    describe('POST Perform an International Transfer', () => {
+      it('should return the request state object with status 202 to indicate that the request is pending', async () => {
+        let quotationReference = 'REF-1636533162268';
+        let quoteId = undefined;
+        const response = await createAIntTransferTransaction(quotationReference, quoteId, false);
+
+        expect(response.status).toBe(202);
+        expect(response.data).toHaveProperty('status');
+        expect(response.data.status).toBe('pending');
+        expect(response.data).toHaveProperty('serverCorrelationId');
+        expect(response.data).toHaveProperty('notificationMethod');
+        expect(response.data.notificationMethod).toBe('callback');
+      });
+    })
+  });
+
+  describe('Perform a International Transfer Reversal', () => {
+    let serverCorrelationId;
+    let objectReference;
+
+    describe('POST Perform an International Transfer', () => {
+      it('should return the request state object with status 202 to indicate that the request is pending', async () => {
+        let quotationReference = 'REF-1636533162268';
+        let quoteId = undefined;
+        const response = await createAIntTransferTransaction(quotationReference, quoteId, false);
+
+        expect(response.status).toBe(202);
+        expect(response.data).toHaveProperty('status');
+        expect(response.data.status).toBe('pending');
+        expect(response.data).toHaveProperty('serverCorrelationId');
+        expect(response.data).toHaveProperty('notificationMethod');
+        expect(response.data.notificationMethod).toBe('callback');
+
+        serverCorrelationId = response.data.serverCorrelationId
+      });
+    })
+
+    describe('GET Poll to Determine the Request State', () => {
+      it('should return the request state object with status 200 for a given server correlation id', async () => {
+        const response = await viewARequestState(serverCorrelationId);
+
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('status');
+        expect(response.data.status).toMatch(/^(pending|completed|failed)$/);
+        expect(response.data).toHaveProperty('serverCorrelationId');
+        expect(response.data).toHaveProperty('notificationMethod');
+        expect(response.data.notificationMethod).toBe('callback');
+        expect(response.data).toHaveProperty('objectReference');
+
+        objectReference = response.data.objectReference;
+      });
+    })
+
+    describe('POST Perform a Transaction Reversal', () => {
+      it('should return the request state object with status 202 to indicate that the request is pending', async () => {
+        const response = await createAReversal(objectReference);
+
+        expect(response.status).toBe(202);
+        expect(response.data).toHaveProperty('status');
+        expect(response.data.status).toBe('pending');
+        expect(response.data).toHaveProperty('serverCorrelationId');
+        expect(response.data).toHaveProperty('notificationMethod');
+        expect(response.data.notificationMethod).toBe('callback');
+      });
+    })
+  });
+
+  describe('Obtain a Financial Service Provider Balance', () => {
+    describe('GET Get an Account Balance', () => {
+      it('should return the balance object with status 200', async () => {
+        const response = await viewAccountBalance('accountid', '2000');
+
+        expect(response.status).toBe(200);
+      });
+    })
+  });
+
+  describe('Retrieve Transactions for a Financial Service Provider', () => {
+    describe('GET Retrieve a Set of Transactions for an Account', () => {
+      it('should return a transactions array of length 20 and indicate via response header how many transactions available in total', async () => {
+        const response = await viewAccountSpecificTransaction('accountid', '2000', 0, 20);
+
+        expect(response.status).toBe(200);
+        expect(response.data.length).toBe(20);
+        expect(response.headers).toHaveProperty('x-records-available-count');
+        expect(response.headers).toHaveProperty('x-records-returned-count');
+      });
+    })
+  });
+
+  describe('Check for API Provider Service Availability', () => {
+    describe('GET Check for Service Availability', () => {
+      it('should return the heartbeat object with status 200 to indicate the status available, unavailable or degraded', async () => {
+        const response = await checkApiAvailability();
+
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('serviceStatus');
+        expect(response.data.serviceStatus).toMatch(/^(available|unavailable|degraded)$/);
+      });
+    })
+  });
+
+  describe('Retrieve a Missing API Response from an API Provider', () => {
+    let clientCorrelationId;
+    let link;
+
+    describe('POST Perform an International Transfer', () => {
+      it('should return the request state object with status 202 to indicate that the request is pending', async () => {
+        let quotationReference = 'REF-1636533162268';
+        let quoteId = undefined;
+        const response = await createAIntTransferTransaction(quotationReference, quoteId, false);
+
+        expect(response.status).toBe(202);
+        expect(response.data).toHaveProperty('status');
+        expect(response.data.status).toBe('pending');
+        expect(response.data).toHaveProperty('serverCorrelationId');
+        expect(response.data).toHaveProperty('notificationMethod');
+        expect(response.data.notificationMethod).toBe('callback');
+
+        clientCorrelationId = response.config.headers['X-CorrelationID']
+      });
+    })
+
+    describe('GET Retrieve a Missing Response', () => {
+      it('should return a response object with status 200 containing a link to the missing resource', async () => {
+        const response = await viewAResponse(clientCorrelationId);
+
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('link');
+
+        link = response.data.link;
+      });
+    })
+
+    describe('GET Retrieve a Missing Resource', () => {
+      it('should return the requested object with status 200 containing a representation of the missing resource', async () => {
+        const response = await viewAResource(link);
+
+        expect(response.status).toBe(200);
+      });
+    })
+  });
 })
